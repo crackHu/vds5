6373edc52823ff1178ae54ce67d97fc8801e5c6a
[crackHu] <[296717367@qq.com]>
2016-09-18 22:17:19 +0800

    无语

---
+++ PROCEDURE/procJBZLInsert.sql
@@ -0,0 +1,21 @@
+DELIMITER $$
+
+USE `phrv2`$$
+
+DROP PROCEDURE IF EXISTS `procJBZLInsert`$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `procJBZLInsert`(IN pageNo INT, IN pageSize INT)
+BEGIN
+  IF(pageNo < 1) 
+  THEN SET @start = 0 ;
+  ELSE SET @start = (pageNo - 1) * pageSize ;
+  END IF ;
+  SET @end = pageSize ;
+  SET @sql = 'SELECT * FROM phr_grda_jbzl limit ?,?' ;
+  PREPARE stmt FROM @sql ;
+  EXECUTE stmt USING @start,
+  @end ;
+  DEALLOCATE PREPARE stmt ;
+END$$
+
+DELIMITER ;
